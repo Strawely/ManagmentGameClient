37f18a3efdc5e554da456df9307e6bffe33bf353
Iverganz <ivan.grev98@gmail.com>
2018-12-21 06:05:23 +0400

    Созданы активити под вкладки и присоединены к вкладкам

---
+++ app/src/main/java/com/example/solom/managmentgame/CompetitorsActivity.java
@@ -0,0 +1,14 @@
+package com.example.solom.managmentgame;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CompetitorsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_competitors);
+    }
+}
--- app/src/main/java/com/example/solom/managmentgame/GameActivity.java
+++ app/src/main/java/com/example/solom/managmentgame/GameActivity.java
@@ -1,13 +1,38 @@
 package com.example.solom.managmentgame;
 
+import android.app.TabActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        // получаем TabHost
+        TabHost tabHost = getTabHost();
+
+        // инициализация была выполнена в getTabHost
+        // метод setup вызывать не нужно
+
+        TabHost.TabSpec tabSpec;
+
+        tabSpec = tabHost.newTabSpec("tag1");
+        tabSpec.setIndicator("Вкладка 1");
+        tabSpec.setContent(new Intent(this, BankActivity.class));
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag2");
+        tabSpec.setIndicator("Вкладка 2");
+        tabSpec.setContent(new Intent(this, FabricActivity.class));
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag3");
+        tabSpec.setIndicator("Вкладка 3");
+        tabSpec.setContent(new Intent(this, CompetitorsActivity.class));
+        tabHost.addTab(tabSpec);
     }
 }
