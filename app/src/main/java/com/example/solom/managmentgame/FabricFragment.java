868ff7b68f16fb0f9d64b38404a42d5764b5e0ee
Groups <33724100+netcrackerGroup@users.noreply.github.com>
2018-12-23 20:11:21 +0400

    Диалоговые окна на нижние кнопки

--- app/src/main/java/com/example/solom/managmentgame/FabricFragment.java
+++ app/src/main/java/com/example/solom/managmentgame/FabricFragment.java
@@ -1,6 +1,7 @@
 package com.example.solom.managmentgame;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
@@ -8,9 +9,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.app.AlertDialog;
+import android.widget.Toast;
 
 import com.example.solom.managmentgame.dataLayer.GameStateHandler;
 import com.example.solom.managmentgame.dataLayer.PlayerState;
@@ -20,12 +24,17 @@ public class FabricFragment extends Fragment {
     private PlayerState playerState;
     ImageView fabric;
     ImageView fabricA;
+    private Context context = getContext();
+    AlertDialog.Builder adB;
+    AlertDialog.Builder adN;
+    AlertDialog.Builder adF;
     Handler handler = new Handler();
     TextView eSM;
     TextView eGP;
     TextView money;
-TextView round;
+    TextView round;
     LinearLayout liner;
+    Button bankrupt,build,next;
 
 
     public FabricFragment() {
@@ -41,23 +50,111 @@ TextView round;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fabric, container, false);
         liner = view.findViewById(R.id.linerFabric);
-        fabric=new ImageView(getActivity());
+        fabric=new ImageView(context);
         fabric.setBackgroundResource(R.drawable.fabric);
-        fabricA=new ImageView(getActivity());
+        fabricA=new ImageView(context);
         fabricA.setBackgroundResource(R.drawable.auto_fabric);
         eSM=view.findViewById(R.id.textViewEsm);
         eGP=view.findViewById(R.id.textViewEgp);
         money=view.findViewById(R.id.textViewMoney);
         round=view.findViewById(R.id.textViewRound);
+        bankrupt=view.findViewById(R.id.floatingActionButtonBankrupt);
+        next=view.findViewById(R.id.floatingActionButtonNext);
+
+        String titleB = "Банкротство";
+        String messageB = "Вы правда хотите объявить о банкротстве?";
+        String button1StringB = "Да";
+
+        String titleN = "Пропустить ход";
+        String messageN = "Вы правда хотите пропустить ход?";
+        String button1StringN = "Да";
+
+        String titleF = "Постройка фабрики";
+        String messageF = "Какую фабрику вы хотит построить?";
+        String button1StringF = "Автоматизированная";
+        String button2StringF="Обычная";
+
+        adB = new AlertDialog.Builder(context);
+        adB.setTitle(titleB);  // заголовок
+        adB.setMessage(messageB); // сообщение
+
+        adN = new AlertDialog.Builder(context);
+        adN.setTitle(titleN);  // заголовок
+        adN.setMessage(messageN); // сообщение
+
+        adF = new AlertDialog.Builder(context);
+        adF.setTitle(titleF);  // заголовок
+        adF.setMessage(messageF); // сообщение
+
+        adB.setPositiveButton(button1StringB, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int arg1) {
+                Toast.makeText(context, "Вы сделали правильный выбор",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        adB.setCancelable(true);
+        adB.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+                Toast.makeText(context, "Вы ничего не выбрали",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        adN.setPositiveButton(button1StringN, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int arg1) {
+                Toast.makeText(context, "Вы сделали правильный выбор",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        adN.setCancelable(true);
+        adN.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+                Toast.makeText(context, "Вы ничего не выбрали",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        adF.setNegativeButton(button2StringF, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int arg1) {
+                Toast.makeText(context, "Возможно вы правы", Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+        adF.setCancelable(true);
+        adF.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+                Toast.makeText(context, "Вы ничего не выбрали",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        bankrupt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adB.show();
+            }
+            });
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adB.show();
+            }
+        });
+
         update();
 
 
         return view;
     }
+
+
+
     protected void update(){
         SocketConnector.updatePlayerState();
         playerState = GameStateHandler.getPlayerState();
-        for(int i=0;i<GameStateHandler.getPlayerState().getFabrics1();i++){
+        for(int i=0;i<playerState.getFabrics1();i++){
             liner.addView(fabric);
         }
         for(int i=0;i<playerState.getFabrics2();i++){
@@ -65,7 +162,7 @@ TextView round;
         }
         eGP.setText(playerState.getEgp());
         eSM.setText(playerState.getEsm());
-        String m=Integer.toString(GameStateHandler.getPlayerState().getMoney())+"$";
+        String m=Integer.toString(playerState.getMoney())+"$";
         money.setText(m);
         round.setText(GameStateHandler.getGame().getTurnNum());
     }
