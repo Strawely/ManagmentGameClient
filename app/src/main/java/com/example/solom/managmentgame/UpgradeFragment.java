50fda40d8235a78f3263f87ce53fabcbff3d5998
Groups <33724100+netcrackerGroup@users.noreply.github.com>
2018-12-24 16:35:37 +0400

    Присобачил строительство

--- app/src/main/java/com/example/solom/managmentgame/UpgradeFragment.java
+++ app/src/main/java/com/example/solom/managmentgame/UpgradeFragment.java
@@ -1,7 +1,5 @@
 package com.example.solom.managmentgame;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -9,18 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RadioButton;
+import android.widget.Switch;
 
 import com.example.solom.managmentgame.dataLayer.SocketConnector;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
-public class BuildFragment extends Fragment {
-
-
-    public BuildFragment() {
+public class UpgradeFragment extends Fragment {
+    public UpgradeFragment() {
         // Required empty public constructor
     }
 
@@ -33,18 +25,17 @@ public class BuildFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_build, container, false);
-        Button button=view.findViewById(R.id.buttonBuild);
-        RadioButton auto=view.findViewById(R.id.radioButtonAuto);
+        View view = inflater.inflate(R.layout.fragment_upgrade, container, false);
+        Button button = view.findViewById(R.id.buttonUp);
+        Switch auto = view.findViewById(R.id.switchUp);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println("===BuildOrderClick===");
+                System.out.println("===UpgradeOrderClick===");
                 button.setEnabled(false);
-                SocketConnector.sendBuildingRequest(auto.isChecked());
+                SocketConnector.sendUpgradeRequest(auto.isChecked());
             }
         });
         return view;
     }
-
 }
